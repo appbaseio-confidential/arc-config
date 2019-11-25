486ac7e1a126661ee69d382086efcd43cd86b72a
lakhansamani <lakhan.m.samani@gmail.com>
2019-11-25 22:07:26 +0530

    chore: use alpine for docker image

--- header.php
+++ header.php
@@ -2,15 +2,11 @@
 <html lang="en">
     <head>
         <title>Arc</title>
-        <link rel="stylesheet" href="https://unpkg.com/purecss@1.0.1/build/pure-min.css" integrity="sha384-oAOxQR6DkCoMliIh8yFnu25d7Eq/PHS21PClpwjOTeU2jRSq11vu66rf90/cZr47" crossorigin="anonymous" />
-        <!--[if lte IE 8]>
-            <link rel="stylesheet" href="https://unpkg.com/purecss@1.0.1/build/grids-responsive-old-ie-min.css">
-        <![endif]-->
-        <!--[if gt IE 8]><!-->
-            <link rel="stylesheet" href="https://unpkg.com/purecss@1.0.1/build/grids-responsive-min.css">
-        <!--<![endif]-->
+        <link rel="stylesheet" href="https://unpkg.com/purecss@1.0.1/build/pure-min.css" integrity="sha384-oAOxQR6DkCoMliIh8yFnu25d7Eq/PHS21PClpwjOTeU2jRSq11vu66rf90/cZr47" crossorigin="anonymous">
+
         <link rel="stylesheet" href="./styles/index.css" />
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <link rel="shortcut icon" href="https://dashboard.appbase.io/static/images/favicon.ico" type="image/x-icon"/>
         <script type='text/javascript' src='./js/index.js'></script>
     </head>
     <body>
