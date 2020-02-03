a09f2157780ba2ec3119b57077eaf80c934f7279
Lakhan Samani <lakhan.m.samani@gmail.com>
2020-02-03 23:21:28 +0530

    fix: post submit

--- post-env.php
+++ post-env.php
@@ -7,6 +7,8 @@
         $data["PASSWORD"] = $_POST["password"];
         $data["ES_CLUSTER_URL"] = $_POST["cluster_url"];
         $data["ARC_ID"] = $_POST["arc_id"];
+        $data["OPENFAAS_GATEWAY"] = $_POST["open_faas_gateway"];
+        $data["OPENFAAS_KUBE_CONFIG"] = $_POST["open_faas_kube_config"];
         if (isset($_POST["username"]) && isset($_POST["password"]) && isset($_POST["cluster_url"]) && isset($_POST["arc_id"]) && !empty($_POST["username"]) && !empty($_POST["username"]) && !empty($_POST["cluster_url"]) && !empty($_POST["arc_id"])) {
             upsertEnvVars($data);
             logout("?success=Data updated successfully");
